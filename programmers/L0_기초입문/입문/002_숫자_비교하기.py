617646a0f30697dc3143da1c8a5178c174b4cb5b
skan0822-kk <skan0822@gmail.com>
2026-01-19 09:56:05 +0900

    [프로그래머스] L0 - 숫자 비교하기 (#2)

---
+++ programmers/L0_기초입문/입문/002_숫자_비교하기.py
@@ -0,0 +1,21 @@
+# 숫자 비교하기
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120807
+# 알고리즘: 기초
+# 작성자: 권남훈
+# 작성일: 2026. 01. 19. 09:56:06
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int num1, int num2) {
+    if(num1 == num2){
+        int answer = 1;
+        return answer;
+    }
+    else{
+        int answer = -1;
+        return answer;
+    }
+}
