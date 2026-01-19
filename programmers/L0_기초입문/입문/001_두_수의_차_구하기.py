686b1efe66ddc1d820e7907f523800c57ed03ed9
skan0822-kk <skan0822@gmail.com>
2026-01-19 09:30:35 +0900

    [프로그래머스] L0 - 두 수의 차 구하기 (#1)

---
+++ programmers/L0_기초입문/입문/001_두_수의_차_구하기.py
@@ -0,0 +1,16 @@
+# 두 수의 차 구하기
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120803
+# 알고리즘: 기초
+# 작성자: 권남훈
+# 작성일: 2026. 01. 19. 09:30:35
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int num1, int num2) {
+    int answer = num1 - num2;
+    return answer;
+}
+
