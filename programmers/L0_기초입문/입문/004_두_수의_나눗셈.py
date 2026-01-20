e16c306e8be91486a905e0d1bbb7e108a5a0779f
skan0822-kk <skan0822@gmail.com>
2026-01-20 11:24:22 +0900

    [프로그래머스] L0 - 두 수의 나눗셈 (#4)

--- programmers/L0_기초입문/입문/004_두_수의_나눗셈.py
+++ programmers/L0_기초입문/입문/004_두_수의_나눗셈.py
@@ -3,7 +3,7 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120806
 # 알고리즘: 기초
 # 작성자: 권남훈
-# 작성일: 2026. 01. 19. 10:06:29
+# 작성일: 2026. 01. 20. 11:24:17
 
 def solution(num1, num2):
     answer = int(num1/num2*1000)
