687220d921ce107a20d464a3b1512c12da6cde02
skan0822-kk <skan0822@gmail.com>
2026-01-21 10:48:29 +0900

    [프로그래머스] L0 - 배열 뒤집기 (#13)

---
+++ programmers/L0_기초입문/입문/013_배열_뒤집기.py
@@ -0,0 +1,10 @@
+# 배열 뒤집기
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120821
+# 알고리즘: 기초
+# 작성자: 권남훈
+# 작성일: 2026. 01. 21. 10:48:26
+
+def solution(num_list):
+    answer = num_list[::-1]
+    return answer
