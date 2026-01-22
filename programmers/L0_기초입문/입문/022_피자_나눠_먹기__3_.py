e32ce9d84146f758878590c32601f7bc5e77a32c
skan0822-kk <skan0822@gmail.com>
2026-01-22 09:06:04 +0900

    [프로그래머스] L0 - 피자 나눠 먹기 (3) (#22)

---
+++ programmers/L0_기초입문/입문/022_피자_나눠_먹기__3_.py
@@ -0,0 +1,12 @@
+# 피자 나눠 먹기 (3)
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120816
+# 알고리즘: 기초
+# 작성자: 권남훈
+# 작성일: 2026. 01. 22. 09:06:07
+
+def solution(slice, n):
+    if n % slice == 0 :
+        return n // slice
+    else :
+        return (n // slice) + 1
